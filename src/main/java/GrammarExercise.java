a77f0852b872cf0843b5bd862ac2a6fd2eae5e16
MSI-PV <805560812@qq.com>
2020-07-29 00:24:37 +0800

    complete java-basic-exercise

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,61 @@
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
     public static void main(String[] args) {
-        //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+
+        Scanner sc=new Scanner(System.in);
+        String firstWordList = sc.nextLine();
+        String secondWordList = sc.nextLine();
 
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
-        //按要求输出到命令行
 
+        System.out.println(result);
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
-        //在这编写实现代码
-        return null;
+
+        String firstWord = firstWordList.toUpperCase();
+        String secondWord = secondWordList.toUpperCase();
+        String[] arr1 = firstWord.split(",");
+        String[] arr2 = secondWord.split(",");
+
+        Arrays.asList(arr1).stream().forEach(x->{if (Pattern.matches("^[a-zA-Z]+$",x) == false){
+            throw new RuntimeException("input not valid");
+        }});
+        Arrays.asList(arr2).stream().forEach(x->{if (Pattern.matches("^[a-zA-Z]+$",x) == false){
+            throw new RuntimeException("input not valid");
+        }});
+
+        String[] arr12 = new String[arr1.length + arr2.length];
+        for (int i = 0; i < arr12.length; i++){
+            if (i<arr1.length){
+                arr12[i]=arr1[i];
+            }else {
+                arr12[i]=arr2[i-arr1.length];
+            }
+        }
+
+        List<String> list = Arrays.asList(arr12);
+        List<String> result2 = new ArrayList<String>();
+        Map<String, Long> collect = list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        collect.forEach((k,v)->{
+            if (v>1){
+                result2.add(k);
+            }
+        });
+        String[] result1 = new String[result2.size()];
+
+        result2.toArray(result1);
+        String split = "(.{1})";
+        String[] array = new String[result1.length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = result1[i].replaceAll(".(?=.)", "$0 ");
+        }
+        List<String> result = Arrays.asList(array);
+
+        return result;
     }
 }
